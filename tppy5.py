10778c1b3a394288a2f8620cf34e047f39d753bb
Nachrj <38332696+Nachrj@users.noreply.github.com>
2019-11-12 20:36:38 -0300

    Listas Enlazadas Terminado
    TODO OK

--- tppy5.py
+++ tppy5.py
@@ -19,6 +19,13 @@ class ListaEnlazada:
         AntCurrent = self.prim
         current = AntCurrent.next
         i = 1
+        # CASO DE ELIMINAR EL UNICO ELEMENTO EN UNA LISTA
+        if self.prim == self.ult:
+            borrado = self.prim.v
+            self.prim = None
+            self.ult = None
+            self.len = 0
+            return borrado
         if index is not None:
             if int(self.len) <= index:
                 raise IndexError("Index out of range")
@@ -59,27 +66,46 @@ class ListaEnlazada:
                 return ix          
             current = current.next
             ix += 1
-        return 
+        return None
     
     def insert(self,index,elem): 
+        if index > self.len:
+            raise IndexError("Index out of range")
         nodo = Nodo(elem)
-        AntCurrent = self.prim
-        current = self.prim.next
-        i = 1
+        # EN CASO DE AGREGAR AL PRINCIPIO DE LA LISTA
         if index == 0:
+            if self.prim is None:
+                self.prim = nodo
+                self.ult = nodo
+                self.len += 1
+                return
             nodo.next = self.prim
             self.prim = nodo
             if self.len == 0:
                 self.ult = nodo
             self.len += 1
             return
+        AntCurrent = self.prim
+        current = self.prim.next
+        i = 1
+        # CASO DE AGREGAR AL FINAL DE LA LISTA
+        if index == self.len:
+            while current is not None:
+                AntCurrent = AntCurrent.next
+                current = current.next
+                i += 1
+            current = nodo
+            self.ult = current
+            AntCurrent.next = self.ult
+            self.len += 1
+            return
+        # CUALQUIER OTRO CASO
         while current is not None:
             if i == index:
-                if index == (self.len - 1):
-                    AntCurrent
-                if current.next is None:
-                    self.ult.next = nodo
-                    self.ult = nodo
+                if i == (self.len - 1):
+                    AntCurrent.next = nodo
+                    AntCurrent.next.next = current
+                    self.ult = current
                     self.len += 1
                     return
                 AntCurrent.next = nodo
@@ -89,9 +115,6 @@ class ListaEnlazada:
             AntCurrent = AntCurrent.next
             current = current.next
             i += 1
-        if self.len <= index:
-            raise IndexError("Index out of range")
-        
         return
     
     def remove(self, elem): 
@@ -134,12 +157,12 @@ class ListaEnlazada:
        for i in range(self.len):
            l.append(current.v)
            current=current.next
-       return repr(l)
+       return str(l)
 
-"""if __name__ == "__main__":
+if __name__ == "__main__":
     Lista = ListaEnlazada()
     Lista.append("Nacho")
     Lista.append("Bruno")
     Lista.append("Manu")
     Lista.append("Fede")
-    print(Lista)"""
+    print(Lista)
